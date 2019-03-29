c48ec9fcb40fc47ed24889a473c29042946dc8d6
nemanjaZivkovic <ser.zhile@gmail.com>
2019-03-29 17:00:03 +0100

    4.41

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken:',accessToken);
-			console.log('refreshToken:',refreshToken);
-			console.log('profile:',profile);
-			console.log('done:::',done);
-		}
-	)
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: [ 'profile', 'email' ] 
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
-//heroku dynamic port for local env use 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
