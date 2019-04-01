a7c94b7bb59cd956cb871c5cb753f304819a6eba
nemanjaZivkovic <ser.zhile@gmail.com>
2019-04-01 16:18:19 +0200

    5.48

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,4 +9,18 @@ module.exports = (app) => {
 	);
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/logout', (req, res) => {
+		// passport also attaches logout to the req object
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		// passport attaches user to the req object
+		res.send(req.user);
+		//res.send(req.session);
+	});
+
+	
 };
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+	console.log('Serializing USER!');
+	done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+	console.log('Deserializing USER!');
+	User.findById(id).then((user) => {
+		done(null, user);
+	});
+});
+
 passport.use(
 	new GoogleStrategy(
 		{
@@ -13,12 +24,17 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		},
 		(accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id })
-                .then((existingUser) => {
-                    if(!existingUser){
-                        new User({ googleId: profile.id }).save();
-                    }
-                });
+			User.findOne({ googleId: profile.id }).then((existingUser) => {
+				if (existingUser) {
+					console.log('User exists', existingUser);
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id }).save().then((user) => {
+						console.log('User created', user);
+						done(null, user);
+					});
+				}
+			});
 		}
 	)
 );
